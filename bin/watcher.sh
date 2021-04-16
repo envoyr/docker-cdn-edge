79b7ba71b4e3c132e7b1f7189482fa2ced32572d
envoyr <hello@envoyr.com>
2021-04-16 20:35:03 +0200

    Update watcher.sh

--- bin/watcher.sh
+++ bin/watcher.sh
@@ -6,8 +6,8 @@ NEW_FILE_LIST=$($LIST_COMMAND)
 while true; do
   if [ "$OLD_FILE_LIST" != "$NEW_FILE_LIST" ]; then
     OLD_FILE_LIST=$NEW_FILE_LIST
-    service nginx reload #> /dev/null &
-    echo "CDN Edge reloaded!"
+    service nginx reload > /dev/null &
+    echo "CDN Edge configuration reloaded!"
   fi
   sleep 2 || exit 2
   NEW_FILE_LIST=$($LIST_COMMAND)
